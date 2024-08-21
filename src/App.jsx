4d0a97b7bb0c71d37e0b1f05b30e95e6260f2cde
Beginnerds <shubhamkalra619@gmail.com>
2024-08-21 12:34:09 +0530

    completed products section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,61 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
-import HeroImg from "./assets/hero.webp"
+import HeroImg from "./assets/hero.webp";
+
+import BUS_ICON from "./assets/bus_icon.svg";
+import PLANE_ICON from "./assets/plane_icon.svg";
+import CODE_ICON from "./assets/code_icon.svg";
+import { useState } from "react";
+
+const PRODUCTS_DATA = [
+  {
+    title: "Transit",
+    icon: BUS_ICON,
+    items: [
+      "Bus Wraps",
+      "Transit Shelters",
+      "Digital Displays",
+      "Tourist Centers",
+      "Exterior Rail",
+    ],
+  },
+  {
+    title: "Air",
+    icon: PLANE_ICON,
+    items: [
+      "Floor Graphics",
+      "Wi-Fi Pages",
+      "Airport Exits",
+      "Check-In Counters",
+      "Terminals",
+    ],
+  },
+  {
+    title: "Digital",
+    icon: CODE_ICON,
+    items: [
+      "Streaming Services",
+      "Affiliate Websites",
+      "Virtual Events",
+      "Mobile Apps",
+      "Search Engines",
+    ],
+  },
+];
 
 function App() {
-
+  const [openProductIndex, setOpenProductIndex] = useState(0);
 
   return (
-    <section className="bg-bg min-h-screen w-screen p-3 lg:p-6 font-primary">
-      <Navbar/>
-      <section className="__HERO_CONTAINER__ relative mt-12 w-full h-[720px] z-10">
-        <div className="__BACK__ absolute -top-4 left-1/2 -translate-x-1/2 w-[90%] z-[-1] h-full bg-primary/50 rounded-3xl">
-
-        </div>
-        <img src={HeroImg} className="h-full w-full object-cover rounded-3xl" alt="" />
+    <section className="bg-bg min-h-screen max-w-screen px-3 py-2 lg:px-6 font-primary text-accent">
+      <Navbar />
+      <section className="__HERO_CONTAINER__ relative mt-8 w-full h-[720px] z-10">
+        <div className="__BACK__ absolute -top-4 left-1/2 -translate-x-1/2 w-[90%] z-[-1] h-full bg-primary/50 rounded-3xl"></div>
+        <img
+          src={HeroImg}
+          className="h-full w-full object-cover rounded-3xl"
+          alt=""
+        />
         <div className="__OVERLAY__ absolute top-0 left-0 h-full w-full pointer-events-none bg-black/30 rounded-3xl"></div>
         <div className="__ITEMS_CONTAINER__ absolute top-0 left-0 h-full w-full rounded-3xl flex flex-col justify-between items-center lg:flex-row lg:items-end py-12 px-8 lg:px-24">
           <div className="__HEADING_CONTAINER__ text-primary">
@@ -25,12 +68,84 @@ function App() {
             <p className="text-primary/80">Suite 1500</p>
             <p className="text-primary/80">Chicao, IL 60652</p>
             <p className="text-primary/80">777-234-1234</p>
-            <a href="#" className="mt-6 w-full bg-primary text-accent font-medium block text-center text-xl rounded-2xl py-3 filter transition-all hover:brightness-90">Contact us</a>
+            <a
+              href="#"
+              className="mt-6 w-full bg-primary text-accent font-medium block text-center text-xl rounded-2xl py-3 filter transition-all hover:brightness-90"
+            >
+              Contact us
+            </a>
           </div>
         </div>
       </section>
+      <section className="__PRODUCTS_CONTAINER__ mt-2 flex flex-col sm:flex-row sm:justify-between bg-primary rounded-3xl px-8 py-12 gap-4 xl:gap-8">
+        <div className="__PRODUCTS_TEXT_CONT__ md:w-1/3 flex flex-col justify-start items-start gap-8">
+          <h2 className="text-4xl font-semibold">Products</h2>
+          <p className="text-accent/80 font-medium">
+            Turn your travel dreams into unforgettable experiences. We provide
+            personalized, seamless service to create journeys that inspire and
+            connect you with the world. Let us help you discover and cherish new
+            adventures.
+          </p>
+        </div>
+        <div className="__ITEMS_CONT__ flex flex-col gap-2 md:w-1/2">
+          {PRODUCTS_DATA.map((item, ind) => {
+            return (
+              <div
+              onClick={()=>setOpenProductIndex(ind)}
+                key={ind}
+                className="bg-bg/40 p-6 rounded-2xl cursor-pointer"
+              >
+                <div className="__TITLE_ICON_CONT__ flex justify-between items-center">
+                  <h3 className="text-xl font-semibold">{item.title}</h3>
+                  <div className="bg-primary rounded-lg size-10 p-2">
+                    <img src={item.icon} alt="" />
+                  </div>
+                </div>
+                {openProductIndex == ind && (
+                  <div className="__ITEMS_ITEMS_CONTAINER__ mt-4 flex flex-wrap  gap-4">
+                    {item.items.map((i) => {
+                      return (
+                        <div
+                          key={i}
+                          className="bg-primary rounded-lg size-full lg:size-40 xl:size-44 2xl:size-52 flex flex-col justify-between items-start px-4 py-2 md:py-4 group hover:bg-accent transition-all hover:text-primary"
+                        >
+                          <svg
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            transform="rotate(90)"
+                            className="max-lg:mb-4 size-4 lg:size-8 stroke-accent group-hover:stroke-primary  "
+                          >
+                            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                            <g
+                              id="SVGRepo_tracerCarrier"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            ></g>
+                            <g id="SVGRepo_iconCarrier">
+                              {" "}
+                              <path
+                                d="M12 5V19M12 5L6 11M12 5L18 11"
+                                
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              ></path>{" "}
+                            </g>
+                          </svg>
+                          <p className="font-semibold">{i}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;

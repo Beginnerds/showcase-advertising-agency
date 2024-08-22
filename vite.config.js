15d99ffd0fa7b9ebdd8a59dbde0fff730e234ac0
Beginnerds <shubhamkalra619@gmail.com>
2024-08-22 12:32:11 +0530

    changed vite base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/showcase-advertising-agency/'
+  base:'https://beginnerds.github.io/showcase-advertising-agency/'
 })
